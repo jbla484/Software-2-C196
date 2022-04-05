cef9992ecbb783ea5f80901d954e2341bb2a36b2
James Blankenship <jbla484@wgu.edu>
2022-04-04 20:47:19 -0700

    Added FXML File.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
         primaryStage.setTitle("Dashboard");
-        primaryStage.setScene(new Scene(root, 900, 400));
+        primaryStage.setScene(new Scene(root, 603, 314));
         primaryStage.show();
     }
 
