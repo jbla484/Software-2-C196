16c88d358de90dbe6ea7fa5cf6e32edc12a646a4
James Blankenship <jbla484@wgu.edu>
2022-04-04 22:14:34 -0700

    Updated the dashboard and added some functionality to the buttons.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+
 public class Controller {
+
+    //Buttons
+    @FXML
+    public Button closeButton = new Button();
+
+    //TextFields
+    @FXML
+    public TextField userID = new TextField();
+    @FXML
+    public TextField userPassword = new TextField();
+
+    //Variables
+    @FXML
+    public String userIDs = "";
+    @FXML
+    public String userPasswords = "";
+
+    @FXML
+    public void initialize() {
+        closeButton.setOnMouseClicked(event -> onCloseButtonClick());
+    }
+
+    @FXML
+    public void checkLoginButton() {
+
+        //Get user input from TextField and save data in variables.
+        userIDs = userID.getText();
+        userPasswords = userPassword.getText();
+
+        System.out.println(userIDs + "\n" + userPasswords);
+    }
+
+    @FXML
+    public void onCloseButtonClick() {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
 }
