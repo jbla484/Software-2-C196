e4c775d7659e778c3f33fc970db6008d2cbeaa10
James Blankenship <jbla484@wgu.edu>
2022-04-08 22:14:49 -0700

    Fixed warnings.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,6 +11,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -80,6 +84,14 @@ public class Controller {
     public String customerPostalCode = "";
     @FXML
     public String customerPhoneNumber = "";
+    @FXML
+    public ObservableList<String> comboBoxValues = FXCollections.observableArrayList();
+    @FXML
+    public ObservableList<String> comboBoxValues0 = FXCollections.observableArrayList();
+    @FXML
+    public ObservableList<String> comboBoxValues1 = FXCollections.observableArrayList();
+    @FXML
+    public ObservableList<String> comboBoxValues2 = FXCollections.observableArrayList();
 
     //ComboBoxes
     @FXML
@@ -118,7 +130,6 @@ public class Controller {
         customerIDText.setText("" + nextCustomerID);
 
         //Populate ComboBox
-        ObservableList<String> comboBoxValues = FXCollections.observableArrayList();
         comboBoxValues.add("Canada");
         comboBoxValues.add("England");
         comboBoxValues.add("United States");
@@ -132,20 +143,18 @@ public class Controller {
 
             if (countryComboBox.getSelectionModel().getSelectedIndex() == 0) {
                 //Populating the first-level-division ComboBox with Canada values.
-                ObservableList<String> comboBoxValues1 = FXCollections.observableArrayList();
-                comboBoxValues1.add("Alberta"); comboBoxValues1.add("British Columbia");
-                comboBoxValues1.add("Manitoba"); comboBoxValues1.add("New Brunswick");
-                comboBoxValues1.add("Newfoundland and Labrador"); comboBoxValues1.add("Nova Scotia");
-                comboBoxValues1.add("Ontario"); comboBoxValues1.add("Prince Edward Island");
-                comboBoxValues1.add("Quebec"); comboBoxValues1.add("Saskatchewan");
+                comboBoxValues0.add("Alberta"); comboBoxValues0.add("British Columbia");
+                comboBoxValues0.add("Manitoba"); comboBoxValues0.add("New Brunswick");
+                comboBoxValues0.add("Newfoundland and Labrador"); comboBoxValues0.add("Nova Scotia");
+                comboBoxValues0.add("Ontario"); comboBoxValues0.add("Prince Edward Island");
+                comboBoxValues0.add("Quebec"); comboBoxValues0.add("Saskatchewan");
 
-                fldComboBox.setItems(comboBoxValues1);
+                fldComboBox.setItems(comboBoxValues0);
             }
             if (countryComboBox.getSelectionModel().getSelectedIndex() == 1) {
                 //Populating the first-level-division ComboBox with England values.
 
                 //FIXME MAY NOT BE THE PROPER VALUES
-                ObservableList<String> comboBoxValues1 = FXCollections.observableArrayList();
                 comboBoxValues1.add("London"); comboBoxValues1.add("North East");
                 comboBoxValues1.add("North West"); comboBoxValues1.add("Yorkshire");
                 comboBoxValues1.add("East Midlands"); comboBoxValues1.add("West Midlands");
@@ -156,34 +165,33 @@ public class Controller {
             }
             if (countryComboBox.getSelectionModel().getSelectedIndex() == 2) {
                 //Populating the first-level-division ComboBox with state values.
-                ObservableList<String> comboBoxValues1 = FXCollections.observableArrayList();
-                comboBoxValues1.add("Alabama"); comboBoxValues1.add("Alaska");
-                comboBoxValues1.add("Arizona"); comboBoxValues1.add("Arkansas");
-                comboBoxValues1.add("California"); comboBoxValues1.add("Colorado");
-                comboBoxValues1.add("Connecticut"); comboBoxValues1.add("Delaware");
-                comboBoxValues1.add("Florida"); comboBoxValues1.add("Georgia");
-                comboBoxValues1.add("Hawaii"); comboBoxValues1.add("Idaho");
-                comboBoxValues1.add("Illinois"); comboBoxValues1.add("Indiana");
-                comboBoxValues1.add("Iowa"); comboBoxValues1.add("Kansas");
-                comboBoxValues1.add("Kentucky"); comboBoxValues1.add("Louisiana");
-                comboBoxValues1.add("Maine"); comboBoxValues1.add("Maryland");
-                comboBoxValues1.add("Massachusetts"); comboBoxValues1.add("Michigan");
-                comboBoxValues1.add("Minnesota"); comboBoxValues1.add("Mississippi");
-                comboBoxValues1.add("Missouri"); comboBoxValues1.add("Montana");
-                comboBoxValues1.add("Nebraska"); comboBoxValues1.add("Nevada");
-                comboBoxValues1.add("New Hampshire"); comboBoxValues1.add("New Jersey");
-                comboBoxValues1.add("New Mexico"); comboBoxValues1.add("New York");
-                comboBoxValues1.add("North Carolina"); comboBoxValues1.add("North Dakota");
-                comboBoxValues1.add("Ohio"); comboBoxValues1.add("Oklahoma");
-                comboBoxValues1.add("Oregon"); comboBoxValues1.add("Pennsylvania");
-                comboBoxValues1.add("Rhode Island"); comboBoxValues1.add("South Carolina");
-                comboBoxValues1.add("South Dakota"); comboBoxValues1.add("Tennessee");
-                comboBoxValues1.add("Texas"); comboBoxValues1.add("Utah");
-                comboBoxValues1.add("Vermont"); comboBoxValues1.add("Virginia");
-                comboBoxValues1.add("Washington"); comboBoxValues1.add("West Virginia");
-                comboBoxValues1.add("Wisconsin"); comboBoxValues1.add("Wyoming");
+                comboBoxValues2.add("Alabama"); comboBoxValues2.add("Alaska");
+                comboBoxValues2.add("Arizona"); comboBoxValues2.add("Arkansas");
+                comboBoxValues2.add("California"); comboBoxValues2.add("Colorado");
+                comboBoxValues2.add("Connecticut"); comboBoxValues2.add("Delaware");
+                comboBoxValues2.add("Florida"); comboBoxValues2.add("Georgia");
+                comboBoxValues2.add("Hawaii"); comboBoxValues2.add("Idaho");
+                comboBoxValues2.add("Illinois"); comboBoxValues2.add("Indiana");
+                comboBoxValues2.add("Iowa"); comboBoxValues2.add("Kansas");
+                comboBoxValues2.add("Kentucky"); comboBoxValues2.add("Louisiana");
+                comboBoxValues2.add("Maine"); comboBoxValues2.add("Maryland");
+                comboBoxValues2.add("Massachusetts"); comboBoxValues2.add("Michigan");
+                comboBoxValues2.add("Minnesota"); comboBoxValues2.add("Mississippi");
+                comboBoxValues2.add("Missouri"); comboBoxValues2.add("Montana");
+                comboBoxValues2.add("Nebraska"); comboBoxValues2.add("Nevada");
+                comboBoxValues2.add("New Hampshire"); comboBoxValues2.add("New Jersey");
+                comboBoxValues2.add("New Mexico"); comboBoxValues2.add("New York");
+                comboBoxValues2.add("North Carolina"); comboBoxValues2.add("North Dakota");
+                comboBoxValues2.add("Ohio"); comboBoxValues2.add("Oklahoma");
+                comboBoxValues2.add("Oregon"); comboBoxValues2.add("Pennsylvania");
+                comboBoxValues2.add("Rhode Island"); comboBoxValues2.add("South Carolina");
+                comboBoxValues2.add("South Dakota"); comboBoxValues2.add("Tennessee");
+                comboBoxValues2.add("Texas"); comboBoxValues2.add("Utah");
+                comboBoxValues2.add("Vermont"); comboBoxValues2.add("Virginia");
+                comboBoxValues2.add("Washington"); comboBoxValues2.add("West Virginia");
+                comboBoxValues2.add("Wisconsin"); comboBoxValues2.add("Wyoming");
 
-                fldComboBox.setItems(comboBoxValues1);
+                fldComboBox.setItems(comboBoxValues2);
             }
         });
     }
@@ -197,43 +205,85 @@ public class Controller {
 
         String query = "Select * FROM users WHERE User_Name = '" + userIDs + "' AND Password = '" + userPasswords + "';";
 
-        Connection connection = JDBC.getConnection();
+        try (Connection connection = JDBC.getConnection();
         Statement statement = connection.createStatement();
-        ResultSet resultset = statement.executeQuery(query);
+        ResultSet resultset = statement.executeQuery(query)) {
 
-        if (!resultset.next()) {
-            if (locale.toString().equals("fr_CA")) {
-                errorDescription.setText("Nom d'utilisateur ou mot de passe invalide.");
+            if (!resultset.next()) {
+                if (locale.toString().equals("fr_CA")) {
+                    errorDescription.setText("Nom d'utilisateur ou mot de passe invalide.");
+                } else {
+                    errorDescription.setText("Invalid username or password.");
+                }
+
             } else {
-                errorDescription.setText("Invalid username or password.");
+
+                errorDescription.setText("Successfully logged in.");
+
+                switchToHome();
+                System.out.println("Logged in!");
             }
 
-        } else {
+            //Find customer ID number for next customer
+            String query2 = "Select Customer_ID FROM customers";
+            ResultSet resultset2 = statement.executeQuery(query2);
 
-            errorDescription.setText("Successfully logged in.");
-
-            switchToHome();
-            System.out.println("Logged in!");
-        }
-
-        //Find customer ID number for next customer
-        String query2 = "Select Customer_ID FROM customers";
-        ResultSet resultset2 = statement.executeQuery(query2);
-
-        while (resultset2.next()) {
-            nextCustomerID++;
+            while (resultset2.next()) {
+                nextCustomerID++;
+            }
         }
     }
 
     @FXML
-    public void handleAddButtonAction() {
+    public void handleAddButtonAction() throws SQLException {
 
         Stage stage = (Stage) addButton.getScene().getWindow();
 
+        //Get input values from TextFields into variables
         customerName = customerNameText.getText();
         customerAddress = customerAddressText.getText();
         customerPostalCode = customerPostalCodeText.getText();
         customerPhoneNumber = customerPhoneNumberText.getText();
+        int countryIndex = countryComboBox.getSelectionModel().getSelectedIndex();
+        int fldIndex = fldComboBox.getSelectionModel().getSelectedIndex();
+        String customerCountry = comboBoxValues.get(countryIndex);
+        String customerFLD = "";
+        if (customerCountry.equals("United States")) {
+            customerFLD = comboBoxValues2.get(fldIndex);
+        }
+        if (customerCountry.equals("England")) {
+            customerFLD = comboBoxValues1.get(fldIndex);
+        }
+        if (customerCountry.equals("Canada")) {
+            customerFLD = comboBoxValues0.get(fldIndex);
+        }
+
+        //Create a query and execute it.
+        //FIXME INSERT INTO TABLE BY ITERATING THROUGH OBSERVABLE LIST
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+        localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        String createdBy = "James";
+        String divisionID = "";
+
+        //Execute a statement to get a division id matching our first-level-division value.
+        String query = "SELECT * FROM first_level_divisions;";
+
+        try (Connection connection = JDBC.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultset = statement.executeQuery(query)) {
+
+        }
+
+        query = "INSERT INTO customers VALUES (" + nextCustomerID + ", " + customerName + ", " +
+                customerAddress + ", " + customerPostalCode + ", " + customerPhoneNumber + ", " +
+                localDateTime + ", " + createdBy + ", " + localDateTime + ", " + createdBy + ", " +
+                divisionID + ");";
+
+
+        System.out.println(query);
 
         //Once done, close
         stage.close();
