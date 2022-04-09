cddeb80cc259dea2bb7c96d9008f79342ef95905
James Blankenship <jbla484@wgu.edu>
2022-04-08 19:33:30 -0700

    Fixed warnings.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,8 +2,6 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
@@ -13,8 +11,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -87,9 +83,9 @@ public class Controller {
 
     //ComboBoxes
     @FXML
-    public ComboBox countryComboBox = new ComboBox();
+    public ComboBox<String> countryComboBox = new ComboBox<>();
     @FXML
-    public ComboBox fldComboBox = new ComboBox();
+    public ComboBox<String> fldComboBox = new ComboBox<>();
 
     @FXML
     public void initialize() {
@@ -129,69 +125,65 @@ public class Controller {
         countryComboBox.setItems(comboBoxValues);
 
         //Set action event for ComboBox
-        countryComboBox.setOnAction(new EventHandler<ActionEvent>() {
+        countryComboBox.setOnAction(event -> {
 
-            @Override
-            public void handle(ActionEvent event) {
+            //Clear first-level-division ComboBox when a new item has been clicked from country ComboBox.
+            fldComboBox.getItems().removeAll(fldComboBox.getItems());
 
-                //Clear first-level-division ComboBox when a new item has been clicked from country ComboBox.
-                fldComboBox.getItems().removeAll(fldComboBox.getItems());
+            if (countryComboBox.getSelectionModel().getSelectedIndex() == 0) {
+                //Populating the first-level-division ComboBox with Canada values.
+                ObservableList<String> comboBoxValues1 = FXCollections.observableArrayList();
+                comboBoxValues1.add("Alberta"); comboBoxValues1.add("British Columbia");
+                comboBoxValues1.add("Manitoba"); comboBoxValues1.add("New Brunswick");
+                comboBoxValues1.add("Newfoundland and Labrador"); comboBoxValues1.add("Nova Scotia");
+                comboBoxValues1.add("Ontario"); comboBoxValues1.add("Prince Edward Island");
+                comboBoxValues1.add("Quebec"); comboBoxValues1.add("Saskatchewan");
 
-                if (countryComboBox.getSelectionModel().getSelectedIndex() == 0) {
-                    //Populating the first-level-division ComboBox with Canada values.
-                    ObservableList<String> comboBoxValues = FXCollections.observableArrayList();
-                    comboBoxValues.add("Alberta"); comboBoxValues.add("British Columbia");
-                    comboBoxValues.add("Manitoba"); comboBoxValues.add("New Brunswick");
-                    comboBoxValues.add("Newfoundland and Labrador"); comboBoxValues.add("Nova Scotia");
-                    comboBoxValues.add("Ontario"); comboBoxValues.add("Prince Edward Island");
-                    comboBoxValues.add("Quebec"); comboBoxValues.add("Saskatchewan");
+                fldComboBox.setItems(comboBoxValues1);
+            }
+            if (countryComboBox.getSelectionModel().getSelectedIndex() == 1) {
+                //Populating the first-level-division ComboBox with England values.
 
-                    fldComboBox.setItems(comboBoxValues);
-                }
-                if (countryComboBox.getSelectionModel().getSelectedIndex() == 1) {
-                    //Populating the first-level-division ComboBox with England values.
+                //FIXME MAY NOT BE THE PROPER VALUES
+                ObservableList<String> comboBoxValues1 = FXCollections.observableArrayList();
+                comboBoxValues1.add("London"); comboBoxValues1.add("North East");
+                comboBoxValues1.add("North West"); comboBoxValues1.add("Yorkshire");
+                comboBoxValues1.add("East Midlands"); comboBoxValues1.add("West Midlands");
+                comboBoxValues1.add("South East"); comboBoxValues1.add("East of England");
+                comboBoxValues1.add("South West");
 
-                    //FIXME MAY NOT BE THE PROPER VALUES
-                    ObservableList<String> comboBoxValues = FXCollections.observableArrayList();
-                    comboBoxValues.add("London"); comboBoxValues.add("North East");
-                    comboBoxValues.add("North West"); comboBoxValues.add("Yorkshire");
-                    comboBoxValues.add("East Midlands"); comboBoxValues.add("West Midlands");
-                    comboBoxValues.add("South East"); comboBoxValues.add("East of England");
-                    comboBoxValues.add("South West");
+                fldComboBox.setItems(comboBoxValues1);
+            }
+            if (countryComboBox.getSelectionModel().getSelectedIndex() == 2) {
+                //Populating the first-level-division ComboBox with state values.
+                ObservableList<String> comboBoxValues1 = FXCollections.observableArrayList();
+                comboBoxValues1.add("Alabama"); comboBoxValues1.add("Alaska");
+                comboBoxValues1.add("Arizona"); comboBoxValues1.add("Arkansas");
+                comboBoxValues1.add("California"); comboBoxValues1.add("Colorado");
+                comboBoxValues1.add("Connecticut"); comboBoxValues1.add("Delaware");
+                comboBoxValues1.add("Florida"); comboBoxValues1.add("Georgia");
+                comboBoxValues1.add("Hawaii"); comboBoxValues1.add("Idaho");
+                comboBoxValues1.add("Illinois"); comboBoxValues1.add("Indiana");
+                comboBoxValues1.add("Iowa"); comboBoxValues1.add("Kansas");
+                comboBoxValues1.add("Kentucky"); comboBoxValues1.add("Louisiana");
+                comboBoxValues1.add("Maine"); comboBoxValues1.add("Maryland");
+                comboBoxValues1.add("Massachusetts"); comboBoxValues1.add("Michigan");
+                comboBoxValues1.add("Minnesota"); comboBoxValues1.add("Mississippi");
+                comboBoxValues1.add("Missouri"); comboBoxValues1.add("Montana");
+                comboBoxValues1.add("Nebraska"); comboBoxValues1.add("Nevada");
+                comboBoxValues1.add("New Hampshire"); comboBoxValues1.add("New Jersey");
+                comboBoxValues1.add("New Mexico"); comboBoxValues1.add("New York");
+                comboBoxValues1.add("North Carolina"); comboBoxValues1.add("North Dakota");
+                comboBoxValues1.add("Ohio"); comboBoxValues1.add("Oklahoma");
+                comboBoxValues1.add("Oregon"); comboBoxValues1.add("Pennsylvania");
+                comboBoxValues1.add("Rhode Island"); comboBoxValues1.add("South Carolina");
+                comboBoxValues1.add("South Dakota"); comboBoxValues1.add("Tennessee");
+                comboBoxValues1.add("Texas"); comboBoxValues1.add("Utah");
+                comboBoxValues1.add("Vermont"); comboBoxValues1.add("Virginia");
+                comboBoxValues1.add("Washington"); comboBoxValues1.add("West Virginia");
+                comboBoxValues1.add("Wisconsin"); comboBoxValues1.add("Wyoming");
 
-                    fldComboBox.setItems(comboBoxValues);
-                }
-                if (countryComboBox.getSelectionModel().getSelectedIndex() == 2) {
-                    //Populating the first-level-division ComboBox with state values.
-                    ObservableList<String> comboBoxValues = FXCollections.observableArrayList();
-                    comboBoxValues.add("Alabama"); comboBoxValues.add("Alaska");
-                    comboBoxValues.add("Arizona"); comboBoxValues.add("Arkansas");
-                    comboBoxValues.add("California"); comboBoxValues.add("Colorado");
-                    comboBoxValues.add("Connecticut"); comboBoxValues.add("Delaware");
-                    comboBoxValues.add("Florida"); comboBoxValues.add("Georgia");
-                    comboBoxValues.add("Hawaii"); comboBoxValues.add("Idaho");
-                    comboBoxValues.add("Illinois"); comboBoxValues.add("Indiana");
-                    comboBoxValues.add("Iowa"); comboBoxValues.add("Kansas");
-                    comboBoxValues.add("Kentucky"); comboBoxValues.add("Louisiana");
-                    comboBoxValues.add("Maine"); comboBoxValues.add("Maryland");
-                    comboBoxValues.add("Massachusetts"); comboBoxValues.add("Michigan");
-                    comboBoxValues.add("Minnesota"); comboBoxValues.add("Mississippi");
-                    comboBoxValues.add("Missouri"); comboBoxValues.add("Montana");
-                    comboBoxValues.add("Nebraska"); comboBoxValues.add("Nevada");
-                    comboBoxValues.add("New Hampshire"); comboBoxValues.add("New Jersey");
-                    comboBoxValues.add("New Mexico"); comboBoxValues.add("New York");
-                    comboBoxValues.add("North Carolina"); comboBoxValues.add("North Dakota");
-                    comboBoxValues.add("Ohio"); comboBoxValues.add("Oklahoma");
-                    comboBoxValues.add("Oregon"); comboBoxValues.add("Pennsylvania");
-                    comboBoxValues.add("Rhode Island"); comboBoxValues.add("South Carolina");
-                    comboBoxValues.add("South Dakota"); comboBoxValues.add("Tennessee");
-                    comboBoxValues.add("Texas"); comboBoxValues.add("Utah");
-                    comboBoxValues.add("Vermont"); comboBoxValues.add("Virginia");
-                    comboBoxValues.add("Washington"); comboBoxValues.add("West Virginia");
-                    comboBoxValues.add("Wisconsin"); comboBoxValues.add("Wyoming");
-
-                    fldComboBox.setItems(comboBoxValues);
-                }
+                fldComboBox.setItems(comboBoxValues1);
             }
         });
     }
@@ -234,7 +226,7 @@ public class Controller {
     }
 
     @FXML
-    public void handleAddButtonAction() throws IOException {
+    public void handleAddButtonAction() {
 
         Stage stage = (Stage) addButton.getScene().getWindow();
 
@@ -242,6 +234,9 @@ public class Controller {
         customerAddress = customerAddressText.getText();
         customerPostalCode = customerPostalCodeText.getText();
         customerPhoneNumber = customerPhoneNumberText.getText();
+
+        //Once done, close
+        stage.close();
     }
 
     @FXML
