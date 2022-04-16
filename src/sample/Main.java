4ce9b244660e0cbce4a63763de56fc79717eba59
James Blankenship <jbla484@wgu.edu>
2022-04-16 16:49:49 -0700

    Added some Javadoc comments.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -103,8 +104,11 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JDBC.makeConnection();
+
+        File loginActivity = new File("login_activity.txt");
+        loginActivity.createNewFile();
 
         launch(args);
 
